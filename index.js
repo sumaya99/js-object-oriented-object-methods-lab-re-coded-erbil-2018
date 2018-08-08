fa6059507fe94b21f2002d588352e09bf903f6ed
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-08 05:46:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function BoardMember (name, homeState, training){
   this.approve= function(){ return 'You can do that!';};
   this.doCharity = function(){ return 'I like to help people.' ;};
   this.releasePressStatement= function(){ return 'You will see great things from Scuber.';} ;
-  this.sayHi= function(){ return 'Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.';};
+  this.sayHi= function(){ return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;};
 }
 let polishedBoardMember = new BoardMember('Polished', 'New York', 'law.');
